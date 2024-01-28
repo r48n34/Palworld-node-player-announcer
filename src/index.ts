4edf31b37e6189ccd189b6f243e1356f7da8a587
reemo <r48n34@gmail.com>
2024-01-28 16:42:47 +0800

    Add announce time code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import { RconClient } from "rconify";
+import dayjs from 'dayjs'
+
 import { numberToFormatStr } from './utils/commonUtils';
 
 process.setMaxListeners(15);
@@ -9,9 +11,9 @@ async function connectFunc(client: RconClient){
 
     // Announce player leave or join 
     async function announceLeaveJoin(diffArr: number, mode: "LEAVE" | "JOIN", maxPLayer: number){
-        const sendText = `${numberToFormatStr(diffArr)}-player-${mode === "LEAVE" ? "leaved" : "joined"}|(Total-${maxPLayer})`;
-        console.log(`${sendText} (${new Date().toUTCString()})`)
-        await client.sendCommand(`Broadcast ${sendText}`);
+        const sendText = `${numberToFormatStr(diffArr)}-player-${mode === "LEAVE" ? "leaved(-)" : "joined(+)"}|(Total-${maxPLayer})`;
+        console.log(`${sendText} (${dayjs().format('DD/MM/YYYY - HH:mm:ss')})`)
+        await client.sendCommand(`Broadcast [${dayjs().format('HH:mm:ss')}]|${sendText}`);
     }
 
     await client.connect();
