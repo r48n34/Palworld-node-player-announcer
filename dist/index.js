1925f84aa327c1078ab587fdc807167ec59e37d9
reemo <r48n34@gmail.com>
2024-01-29 21:53:41 +0800

    Fix minor issues

--- dist/index.js
+++ dist/index.js
@@ -8,58 +8,66 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const rconify_1 = require("rconify");
+const dayjs_1 = __importDefault(require("dayjs"));
 const commonUtils_1 = require("./utils/commonUtils");
 process.setMaxListeners(15);
 let currentPlayerCount = -1;
 function connectFunc(client) {
     return __awaiter(this, void 0, void 0, function* () {
-        // Announce player leave or join 
-        function announceLeaveJoin(diffArr, mode, maxPLayer) {
-            return __awaiter(this, void 0, void 0, function* () {
-                const sendText = `${(0, commonUtils_1.numberToFormatStr)(diffArr)}-player-${mode === "LEAVE" ? "leaved" : "joined"}|(Total-${maxPLayer})`;
-                console.log(`${sendText} (${new Date().toUTCString()})`);
-                yield client.sendCommand(`Broadcast ${sendText}`);
-            });
-        }
-        yield client.connect();
-        const result = yield client.sendCommand("ShowPlayers");
-        const nowPlayer = result.split("\n").slice(1);
-        if (currentPlayerCount === -1) {
-            currentPlayerCount = nowPlayer.length;
-        }
-        else if (currentPlayerCount !== nowPlayer.length) {
-            const diff = Math.abs(nowPlayer.length - currentPlayerCount);
-            if (nowPlayer.length > currentPlayerCount) { // New player join
-                yield announceLeaveJoin(diff, "JOIN", nowPlayer.length);
+        try {
+            // Announce player leave or join 
+            function announceLeaveJoin(diffArr, mode, maxPLayer) {
+                return __awaiter(this, void 0, void 0, function* () {
+                    const sendText = `${(0, commonUtils_1.numberToFormatStr)(diffArr)}-player-${mode === "LEAVE" ? "leaved(-)" : "joined(+)"}|(Total-${maxPLayer})`;
+                    console.log(`${sendText} (${(0, dayjs_1.default)().format('DD/MM/YYYY - HH:mm:ss')})`);
+                    yield client.sendCommand(`Broadcast [${(0, dayjs_1.default)().format('HH:mm:ss')}]|${sendText}`);
+                });
             }
-            else { // Player left    
-                yield announceLeaveJoin(diff, "LEAVE", nowPlayer.length);
+            yield client.connect();
+            const result = yield client.sendCommand("ShowPlayers");
+            const nowPlayer = result.split("\n").slice(1);
+            if (currentPlayerCount === -1) {
+                currentPlayerCount = nowPlayer.length;
             }
-            currentPlayerCount = nowPlayer.length;
+            else if (currentPlayerCount !== nowPlayer.length) {
+                const diff = Math.abs(nowPlayer.length - currentPlayerCount);
+                if (nowPlayer.length > currentPlayerCount) { // New player join
+                    yield announceLeaveJoin(diff, "JOIN", nowPlayer.length);
+                }
+                else { // Player left    
+                    yield announceLeaveJoin(diff, "LEAVE", nowPlayer.length);
+                }
+                currentPlayerCount = nowPlayer.length;
+            }
+            client.disconnect();
         }
-        client.disconnect();
+        catch (error) {
+            console.error(error);
+        }
     });
 }
 function main() {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        const INTERVAL = process.env.INTERVAL ? +process.env.INTERVAL * 1000 : 5 * 1000;
-        const client = new rconify_1.RconClient({
-            host: (_a = process.env.RCON_IP) !== null && _a !== void 0 ? _a : "127.0.0.1",
-            port: process.env.RCON_PORT ? +process.env.RCON_PORT : 25575,
-            password: process.env.RCON_PASSWORD || "",
-            ignoreInvalidAuthResponse: false
-        });
         try {
+            const INTERVAL = process.env.INTERVAL ? +process.env.INTERVAL * 1000 : 5 * 1000;
+            const client = new rconify_1.RconClient({
+                host: (_a = process.env.RCON_IP) !== null && _a !== void 0 ? _a : "127.0.0.1",
+                port: process.env.RCON_PORT ? +process.env.RCON_PORT : 25575,
+                password: process.env.RCON_PASSWORD || "",
+                ignoreInvalidAuthResponse: false
+            });
             console.log("Connected, server starting");
             setInterval(() => __awaiter(this, void 0, void 0, function* () { return connectFunc(client); }), INTERVAL);
         }
         catch (error) {
             console.error(error);
-            client.disconnect();
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -9,55 +9,61 @@ let currentPlayerCount: number = -1;
 
 async function connectFunc(client: RconClient){
 
-    // Announce player leave or join 
-    async function announceLeaveJoin(diffArr: number, mode: "LEAVE" | "JOIN", maxPLayer: number){
-        const sendText = `${numberToFormatStr(diffArr)}-player-${mode === "LEAVE" ? "leaved(-)" : "joined(+)"}|(Total-${maxPLayer})`;
-        console.log(`${sendText} (${dayjs().format('DD/MM/YYYY - HH:mm:ss')})`)
-        await client.sendCommand(`Broadcast [${dayjs().format('HH:mm:ss')}]|${sendText}`);
-    }
-
-    await client.connect();
-
-    const result: string = await client.sendCommand("ShowPlayers");
-    const nowPlayer: string[] = result.split("\n").slice(1)
-
-    if(currentPlayerCount === -1){
-        currentPlayerCount = nowPlayer.length
-    }
-    else if(currentPlayerCount !== nowPlayer.length) {
-        const diff = Math.abs(nowPlayer.length - currentPlayerCount)
+    try {
         
-        if(nowPlayer.length > currentPlayerCount){ // New player join
-            await announceLeaveJoin(diff, "JOIN", nowPlayer.length)
+        // Announce player leave or join 
+        async function announceLeaveJoin(diffArr: number, mode: "LEAVE" | "JOIN", maxPLayer: number){
+            const sendText = `${numberToFormatStr(diffArr)}-player-${mode === "LEAVE" ? "leaved(-)" : "joined(+)"}|(Total-${maxPLayer})`;
+            console.log(`${sendText} (${dayjs().format('DD/MM/YYYY - HH:mm:ss')})`)
+            await client.sendCommand(`Broadcast [${dayjs().format('HH:mm:ss')}]|${sendText}`);
         }
-        else { // Player left    
-            await announceLeaveJoin(diff, "LEAVE", nowPlayer.length)
+    
+        await client.connect();
+    
+        const result: string = await client.sendCommand("ShowPlayers");
+        const nowPlayer: string[] = result.split("\n").slice(1)
+    
+        if(currentPlayerCount === -1){
+            currentPlayerCount = nowPlayer.length
         }
-
-        currentPlayerCount = nowPlayer.length
+        else if(currentPlayerCount !== nowPlayer.length) {
+            const diff = Math.abs(nowPlayer.length - currentPlayerCount)
+            
+            if(nowPlayer.length > currentPlayerCount){ // New player join
+                await announceLeaveJoin(diff, "JOIN", nowPlayer.length)
+            }
+            else { // Player left    
+                await announceLeaveJoin(diff, "LEAVE", nowPlayer.length)
+            }
+    
+            currentPlayerCount = nowPlayer.length
+        }
+    
+        client.disconnect();      
+    }
+    catch (error) {
+        console.error(error)
     }
 
-    client.disconnect();      
 }
 
 async function main(){
 
-    const INTERVAL = process.env.INTERVAL ? +process.env.INTERVAL * 1000 : 5 * 1000;
-
-    const client: RconClient = new RconClient({
-        host: process.env.RCON_IP ?? "127.0.0.1",
-        port: process.env.RCON_PORT ? +process.env.RCON_PORT : 25575,
-        password: process.env.RCON_PASSWORD || "",
-        ignoreInvalidAuthResponse: false
-    });
-
     try {
+        const INTERVAL = process.env.INTERVAL ? +process.env.INTERVAL * 1000 : 5 * 1000;
+
+        const client: RconClient = new RconClient({
+            host: process.env.RCON_IP ?? "127.0.0.1",
+            port: process.env.RCON_PORT ? +process.env.RCON_PORT : 25575,
+            password: process.env.RCON_PASSWORD || "",
+            ignoreInvalidAuthResponse: false
+        });
+
         console.log("Connected, server starting")
         setInterval( async () => connectFunc(client), INTERVAL) 
     }
     catch (error) {
         console.error(error)
-        client.disconnect()
     }
     
 }
